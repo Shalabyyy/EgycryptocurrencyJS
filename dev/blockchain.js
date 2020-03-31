19fe9a9d47d6d055b479a20cd9208fa0cac5d0ff
Shalabyyy <youshalaby@gmail.com>
2020-03-31 16:18:50 +0200

    Deploying Node 4

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -6,7 +6,7 @@ const currentNodeUrl = getUrl();
 
 function getUrl() {
   if (process.env.NODE_ENV === "production") {
-    return "https://egycryptocurrency-node-3.herokuapp.com";
+    return "https://egycryptocurrency-node-4.herokuapp.com";
   } else {
     return process.argv[3];
   }
