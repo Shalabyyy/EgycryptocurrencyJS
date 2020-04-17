b6930838236adda800a7fc4f72448acd6d04d6c8
Shalabyyy <youshalaby@gmail.com>
2020-04-17 17:36:49 +0200

    Deploying wallet Take 3

--- dev/wallet/wallet.js
+++ dev/wallet/wallet.js
@@ -195,6 +195,14 @@ app.get("/logout", function(req, res) {
   res.status(200).send({ auth: false, token: null });
 });
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+  
+}
+
 app.listen(port, () => {
   console.log(`Wallet running on port ${port}`);
 });
