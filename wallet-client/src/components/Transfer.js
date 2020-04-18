343f5a103be9a0512362bb9c53534d3e164faf29
Shalabyyy <youshalaby@gmail.com>
2020-04-18 17:05:26 +0200

    Added Table Feature anf Fixed Bugs

--- wallet-client/src/components/Transfer.js
+++ wallet-client/src/components/Transfer.js
@@ -1,15 +1,17 @@
 import React, { Component } from "react";
 import rp from "request-promise";
 import { Redirect } from "react-router-dom";
+import M from "materialize-css";
 class Transfer extends Component {
   state = {
     account: {},
     jwt: "",
     balance: 0.0,
     recipient: "",
-    amount: "",
+    amount: 0,
     height: 0,
-    width: 0
+    width: 0,
+    refresh:false
   };
   componentDidMount() {
     const { width, height } = this.getWindowDimension();
@@ -21,7 +23,7 @@ class Transfer extends Component {
     });
     console.log(this.props.location.state);
     const requestOptions = {
-      uri: "https://boxcoin-wallet.herokuapp.com//get-balance",
+      uri: "https://boxcoin-wallet.herokuapp.com/get-balance",
       method: "GET",
       json: true,
       headers: {
@@ -30,8 +32,38 @@ class Transfer extends Component {
     };
     rp(requestOptions)
       .then(data => {
-        console.log(data.message.result.balance);
+        console.log(data.message.result);
         this.setState({ balance: data.message.result.balance });
+        if (data.message.result.transactions.length > 0) {
+          data.message.result.transactions.forEach(transaction => {
+            const amount = transaction.amount;
+            var sender = transaction.sender;
+            var recipient = transaction.recipient;
+            var style = "";
+            const myAddress = this.props.location.state.account.publicAddress;
+            if (myAddress === sender) {
+              sender = "ME";
+              style = "#ff9999";
+            }
+            if (myAddress === recipient) {
+              recipient = "ME";
+              style = "#ccffcc";
+            }
+
+            var tableData = document.getElementById("table-body").innerHTML;
+            document.getElementById("table-body").innerHTML =
+              tableData +
+              `<tr style="background-color:${style}">
+          <td>${sender}</td>
+          <td>${recipient}</td>
+          <td>${amount} BOX</td>
+        </tr>`;
+          });
+        } else {
+          var tableData = document.getElementById("table-body").innerHTML;
+          document.getElementById("table-body").innerHTML =
+            "<tr> <td colspan='3'>No Transactions Were Made</td></tr>";
+        }
       })
       .catch(error => console.log(error));
   }
@@ -60,12 +92,18 @@ class Transfer extends Component {
     });
     //console.log(this.state.username)
   };
+  clearTransfer = () => {
+    this.setState({ recipient: "", amount: 0 });
+  };
   transfer = event => {
-    if(this.state.amount<=0 || this.state.recipient.length !==64){
-      window.alert("Please Enter Corret Data")
-      return 
+    if (this.state.amount <= 0 || this.state.recipient.length !== 64) {
+      var toastHTML = "<span>Please Enter Corret Data</span>";
+      M.toast({ html: toastHTML });
+      this.clearTransfer();
+      // window.alert("Please Enter Corret Data");
+      return;
     }
-    
+
     const requestOptions = {
       uri: "https://boxcoin-wallet.herokuapp.com/send-funds",
       method: "POST",
@@ -73,65 +111,102 @@ class Transfer extends Component {
       headers: {
         "x-access-token": this.props.location.state.jwt
       },
-      body: { amount: this.state.amount, recipient: this.state.recipient }
+      body: { amount: Number(this.state.amount), recipient: this.state.recipient }
     };
     if (this.state.amount > this.state.balance) {
-      window.alert("Insuffcient funds");
+      //window.alert("Insuffcient funds");
+      var toastHTML = "<span>Insuffcient funds</span>";
+      M.toast({ html: toastHTML });
+      this.clearTransfer();
       return;
     } else {
       rp(requestOptions)
         .then(data => {
-          window.alert("Your Transaction will be proccessed shortly");
+          //window.alert("Your Transaction will be proccessed shortly");
+          var toastHTML =
+            "<span>Your Transaction will be proccessed shortly</span>";
+          M.toast({ html: toastHTML });
+          this.clearTransfer();
+          window.location.reload()
         })
-        .catch(err => window.alert("Transaction Failed"));
+        .catch(err => {
+          var toastHTML = "<span>Transaction Failed</span>";
+          M.toast({ html: toastHTML });
+          this.clearTransfer();
+        });
     }
   };
   render() {
     if (this.props.location.state.jwt != "")
-      if (this.state.width < 450) return (<div>
-        <div className="container">
-          <p>Welcome {this.props.location.state.account.privateAddress}</p>
-          <h6>Your Balance is: {this.state.balance} BOX</h6>
-          <br></br>
-          <div class="row">
-            <h6>Transfer crypto to anyone in the network</h6>
-            <div class="input-field">
-              <i class="material-icons prefix">verified_user</i>
-              <input
-                id="icon_vu1"
-                type="text"
-                class="validate"
-                name="recipient"
-                value={this.state.recipient}
-                onChange={this.handleChange}
-              />
-              <label for="icon_vu1">recipient</label>
+      if (this.state.width < 450)
+        return (
+          <div>
+            <div className="container">
+              <p>Welcome {this.props.location.state.account.publicAddress}</p>
+              <h6>Your Balance is: {this.state.balance} BOX</h6>
+              <br></br>
+              <div class="row">
+                <h6>Transfer crypto to anyone in the network</h6>
+                <div class="input-field">
+                  <i class="material-icons prefix">verified_user</i>
+                  <input
+                    id="icon_vu1"
+                    type="text"
+                    class="validate"
+                    name="recipient"
+                    value={this.state.recipient}
+                    onChange={this.handleChange}
+                  />
+                  <label for="icon_vu1">recipient</label>
+                </div>
+                <div class="input-field">
+                  <i class="material-icons prefix">lock</i>
+                  <input
+                    id="icon_pwd5"
+                    type="number"
+                    class="validate"
+                    name="amount"
+                    value={this.state.amount}
+                    onChange={this.handleChange}
+                  />
+                  <label for="icon_pwd5">Amount</label>
+                </div>
+                <div class="input-field">
+                  <a
+                    class="btn waves-effect waves-light"
+                    onClick={this.transfer}
+                  >
+                    Transfer
+                    <i class="material-icons right">send</i>
+                  </a>
+                </div>
+              </div>
             </div>
-            <div class="input-field">
-              <i class="material-icons prefix">lock</i>
-              <input
-                id="icon_pwd5"
-                type="number"
-                class="validate"
-                name="amount"
-                value={this.state.amount}
-                onChange={this.handleChange}
-              />
-              <label for="icon_pwd5">Amount</label>
-            </div>
-            <div class="input-field">
-              <a class="btn waves-effect waves-light" onClick={this.transfer}>
-                Transfer
-                <i class="material-icons right">send</i>
-              </a>
+            <div>
+              <h6>Transaction History</h6>
+
+              <table class="highlight responsive-table">
+                <thead>
+                  <tr>
+                    <th>Sender</th>
+                    <th>Recipient</th>
+                    <th>Amount</th>
+                  </tr>
+                </thead>
+
+                <tbody id="table-body"></tbody>
+              </table>
             </div>
           </div>
-        </div>
-      </div>);
+        );
       else
         return (
           <div className="container">
-            <h2>Welcome {this.props.location.state.account.privateAddress}</h2>
+            <h6>Welcome</h6>
+            <p style={{ fontSize: "10px" }}>
+              {" "}
+              {this.props.location.state.account.publicAddress}
+            </p>
             <h3>Your Balance is: {this.state.balance} BOX</h3>
             <div class="row">
               <h6>Transfer crypto to anyone in the network</h6>
@@ -165,6 +240,21 @@ class Transfer extends Component {
                   <i class="material-icons right">send</i>
                 </a>
               </div>
+            </div>
+            <div>
+              <h6>Transaction History</h6>
+
+              <table class="highlight responsive-table">
+                <thead>
+                  <tr>
+                    <th>Sender</th>
+                    <th>Recipient</th>
+                    <th>Amount</th>
+                  </tr>
+                </thead>
+
+                <tbody id="table-body"></tbody>
+              </table>
             </div>
           </div>
         );
