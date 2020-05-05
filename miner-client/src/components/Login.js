67b6b1d3f27e03be41aeb164646b721ef599405a
Shalabyyy <youshalaby@gmail.com>
2020-05-05 17:09:26 +0200

    Completed Miner React App

--- miner-client/src/components/Login.js
+++ miner-client/src/components/Login.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import rp from "request-promise";
 import M from "materialize-css";
 import { Redirect } from "react-router-dom";
+import logo from "../media/logo_transparent.png";
 
 class Login extends Component {
   state = {
@@ -11,18 +12,37 @@ class Login extends Component {
     redirect: false,
     height: 0,
     width: 0,
-    account:[]
+    account: [],
+    loading: false
   };
+  getLoadingHtml =()=>{
+    return ` <div class="preloader-wrapper big active">
+    <div class="spinner-layer spinner-blue-only">
+      <div class="circle-clipper left">
+        <div class="circle"></div>
+      </div><div class="gap-patch">
+        <div class="circle"></div>
+      </div><div class="circle-clipper right">
+        <div class="circle"></div>
+      </div>
+    </div>
+  </div>`
+   };
   handleChange = event => {
     console.log(event.target.value);
     const { name, value } = event.target;
     this.setState({
       [name]: value
     });
-    //console.log(this.state.username)
+    if (this.state.loading) {
+      document.getElementById("ld12").innerHTML = this.getLoadingHtml();
+    } else {
+      document.getElementById("ld12").innerHTML = "";
+    }
   };
   login = event => {
-      console.log("Starting Log In")
+    console.log("Starting Log In");
+    this.setState({ loading: true });
     const requestOptions = {
       uri: "https://boxcoin-wallet.herokuapp.com/login",
       method: "POST",
@@ -39,14 +59,17 @@ class Login extends Component {
           this.setState({
             jwt: data.token,
             account: data.account[0],
-            redirect: true
+            redirect: true,
+            loading: false
           });
         } else {
+          this.setState({ loading: false });
           var toastHTML = "<span>wrong address or password</span>";
           M.toast({ html: toastHTML });
         }
       })
       .catch(err => {
+        this.setState({ loading: false });
         var toastHTML = "<span>Something went wrong</span>";
         M.toast({ html: toastHTML });
       });
@@ -57,13 +80,15 @@ class Login extends Component {
         <Redirect
           to={{
             pathname: "/dashboard",
-            state: { jwt: this.state.jwt,account: this.state.account}
+            state: { jwt: this.state.jwt, account: this.state.account }
           }}
         />
       );
     return (
       <div class="container">
-          <h4>Welcome to BoxCoin Miner App</h4>
+        <img src={logo} width="200px" height="200px"></img>
+
+        <h4>Welcome to BoxCoin Miner App</h4>
         <div id="Row1">
           <div class="row">
             <div class="input-field">
@@ -101,6 +126,7 @@ class Login extends Component {
               </a>
             </div>
           </div>
+          <div id="ld12"></div>
         </div>
       </div>
     );
--- miner-client/src/components/Status.js
+++ miner-client/src/components/Status.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import rp from "request-promise";
 import M from "materialize-css";
+import logo from "../media/logo_transparent.png"
 import { Redirect } from "react-router-dom";
 
 class Status extends Component {
@@ -8,10 +9,22 @@ class Status extends Component {
     jwt: "",
     url1: "",
     status1: "Offline",
-    url2: "",
-   
+    url2: ""
   };
-  componentDidMount(){
+  getLoadingHtml =()=>{
+   return ` <div class="preloader-wrapper big active">
+   <div class="spinner-layer spinner-blue-only">
+     <div class="circle-clipper left">
+       <div class="circle"></div>
+     </div><div class="gap-patch">
+       <div class="circle"></div>
+     </div><div class="circle-clipper right">
+       <div class="circle"></div>
+     </div>
+   </div>
+ </div>`
+  }
+  componentDidMount() {
     const requestOptions = {
       uri: "https://boxcoin-wallet.herokuapp.com/get-node-link",
       method: "GET",
@@ -21,14 +34,18 @@ class Status extends Component {
       }
     };
     rp(requestOptions)
-    .then(data =>{
-      this.setState({url1:data.nodeUrl})
-    })
-    .catch(err=>console.log(err))
+      .then(data => {
+       // console.log(data.nodeUrl[0].nodeUrl);
+        this.setState({ url1: data.nodeUrl[0].nodeUrl });
+        console.log(this.state.url1)
+        document.getElementById("mzrxx22").innerHTML = ""
+        this.setState({status1:"Saved"})
+      })
+      .catch(err => console.log(err));
   }
   addNode = () => {
     console.log("hello Node");
-    this.setState({ status1: "Loading.." });
+    this.setState({ status1: "This Might take a while" });
     const requestOptions = {
       uri: this.state.url1,
       method: "GET",
@@ -46,27 +63,36 @@ class Status extends Component {
             uri: `https://egycryptocurrency-node-${nodeNumber}.herokuapp.com/register-and-broadcast-node`,
             method: "POST",
             body: {
-              newNodeUrl: this.state.url1,
+              newNodeUrl: this.state.url1
             },
             json: true
           };
           rp(request2Options)
-          .then(data=>{
-            this.setState({status1:"Online"})
-            const request3Options = {
-              uri: "https://boxcoin-wallet.herokuapp.com/get-node-link",
-              method: "PATCH",
-              json: true,
-              headers: {
-                "x-access-token": this.props.location.state.jwt
-              },
-              body:{newNodeUrl:this.state.url1}
-            }
-          })
-          .catch(err=>{
-            console.log(err)
-            this.setState({status1:"Network Error"})
-          })
+            .then(data => {
+              this.setState({ status1: "Secure" });
+              const request3Options = {
+                uri: "https://boxcoin-wallet.herokuapp.com/fix-link",
+                method: "PATCH",
+                json: true,
+                headers: {
+                  "x-access-token": this.props.location.state.jwt
+                },
+                body: { newNodeUrl: this.state.url1 }
+              };
+              rp(request3Options)
+                .then(data => {
+                  this.setState({ status1: "Online" });
+                  console.log(data);
+                })
+                .catch(err => {
+                  console.log(err);
+                  this.setState({ status1: "Registeration Error" });
+                });
+            })
+            .catch(err => {
+              console.log(err);
+              this.setState({ status1: "Network Error" });
+            });
         } else {
           this.setState({ status1: "Error" });
         }
@@ -88,11 +114,12 @@ class Status extends Component {
   render() {
     return (
       <div class="container">
-        <h5>
+        <img src={logo} width="200px" height="200px"></img>
+        <h6>
           To ensure that the network remains active, we encourage miners to use
           cloud based computaional power
-        </h5>
-        <h6>Each User is allowed up to 2 Cloud Based Nodes</h6>
+        </h6>
+        <h6>Each User is allowed One Cloud Based Nodes</h6>
         <div>
           <div class="input-field">
             <i class="material-icons prefix">cloud</i>
@@ -101,19 +128,20 @@ class Status extends Component {
               type="text"
               class="validate"
               name="url1"
+              value={this.state.url1}
               onChange={this.handleChange}
             />
-            <label for="icon_prefix55">First URl as (http://website.com)</label>
+            <label for="icon_prefix55" id="mzrxx22">URl as (http://website.com)</label>
           </div>
           <div class="row">
-            <div class="col s6">
+            <div class="col s2">
               <a class="btn waves-effect waves-light" onClick={this.addNode}>
                 Activate
                 <i class="material-icons right">cloud_upload</i>
               </a>
             </div>
             <div class="col s2">
-              <p>Status:{this.state.status1}</p>
+              <h6>Status:{this.state.status1}</h6>
             </div>
           </div>{" "}
         </div>
